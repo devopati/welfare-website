22c0734064d3a262cc7fcf8232a61d3cb06bc114
= <=>
2024-07-19 09:38:40 +0300

    update

--- src/pages/information/BusinessInfo.tsx
+++ src/pages/information/BusinessInfo.tsx
@@ -81,14 +81,11 @@ const BusinessInfo:React.FC = () => {
       <button onClick={updateDataHandler} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full" type="button">
         Continue
       </button>
-    
     </div>
-
-   
   </form>
  
-</div>
     </div>
+  </div>
   )
 }
 
